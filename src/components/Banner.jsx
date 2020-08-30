d7eae9e017e6a4d09f4c2a45e16dfb3205fe9349
nwease <nickwease@gmail.com>
2020-08-30 14:19:02 +0200

    Working on Home and Banner.

---
+++ src/components/Banner.jsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import '../css/Banner.css';
+
+const Banner = () => {
+    return (
+        <div className='banner'>
+            <div className='banner__information'>
+                <h1>
+                    Get out and stretch your imagination
+                </h1>
+
+                <h5>
+                    Plan a different kind of getaway to uncover the hidden gems near you.
+                </h5>
+            </div>
+        </div>
+    );
+};
+
+export default Banner;
--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,10 +1,12 @@
 import React from 'react';
 import '../css/Home.css';
+import Banner from './Banner';
 
 const Home = () => {
     return (
         <div className='home'>
             HOME
+            <Banner />
         </div>
     );
 };
